e7dc2f9482e1c11363eb4c5bb286dfc646c754c4
Valli81106 <sreevalliiscute1234@gmail.com>
2025-09-04 21:28:24 +0530

    feat: added tracks & faqs

---
+++ components/faqData.js
@@ -0,0 +1,37 @@
+const faqData = [
+    {
+        question: "What is Ossome Hacks 3.0?",
+        answer: "Ossome Hacks 3.0 is a 36-hour offline hackathon organized by GitHub Community SRM. It is designed to encourage innovation and problem-solving among SRM students in tracks like healthcare, open innovation, web3 & crypto, edtech and agritech."
+    },
+    {
+        question: "Is there any registration fee?",
+        answer: "No, participation in Ossome Hacks 3.0 is completely free."
+    },
+    {
+        question: "What is the team size limit?",
+        answer: "Teams can consist of 2 to 4 members. Teams from all colleges and universities are allowed to participate."
+    },
+    {
+        question: "How many tracks are there in the hackathon?",
+        answer: "There are five tracks in total: healthcare, edtech, agritech, web3 & crypto, and open innovation."
+    },
+    {
+        question: "Can beginners participate in the hackathon?",
+        answer: "Absolutely! The event is designed to support all experience levels. Even if you know nothing about hackathons, you’ll be learning loads while you work on your project!"
+    },
+    {
+        question: "How do I register for the hackathon?",
+        answer: "You can register yourself through the official website of the Ossome Hacks site and Devfolio. Fill in the required details and confirm your participation."
+    },
+    {
+        question: "Who can I contact if I have more questions?",
+        answer: "Feel free to reach out to us at community@githubsrmist.tech or join our discord server through the GitHub Community SRM website for updates."
+    },
+    {
+        question: "Can we choose more than one track?",
+        answer: "No, each team must select a single track to focus on for their project."
+    }
+
+];
+
+export default faqData;
