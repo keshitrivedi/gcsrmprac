3c115073bf0175a342c5c203047d6c23d7f2536b
Valli81106 <sreevalliiscute1234@gmail.com>
2025-09-04 23:46:56 +0530

    feat: schedule added

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 export default function Hero () {
     return (
     <>
-        <div className="bg-amber-500">Yawrr mujhe nankhatai chahiyeee</div>
+        
     </>
     )
 }
